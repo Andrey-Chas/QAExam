d482d0430dc3b23b56b0859e923a7b81a2073000
Andrey Chasovskoy <andrchas777@gmail.com>
2022-05-07 17:45:22 +0300

    RemovedUnusedImport

--- testing_context_menu.py
+++ testing_context_menu.py
@@ -3,7 +3,6 @@ import unittest
 
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
